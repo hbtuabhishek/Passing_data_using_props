111ef22927fa0fc08a9e65f4fdb79071d901ed01
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-09-18 15:29:52 +0530

    manupulate props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,22 +2,24 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props)
 {
-    const expenseDate=new Date(2023, 3, 1);
-        const expenseTitle='Car Insurance'
-        const expenseAmount=250;
-        const LocationofExpenditure="Delhi";
-    return (
+    const month = props.date.toLocaleString('en-US', {month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+   return (
          
     <div className="expense-item">
         
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
                 <div>{props.location}</div>
             </div>
-        
-        
     </div>
     );
 }
