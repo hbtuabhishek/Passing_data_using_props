7082984bcc70493b5d229e547ce3e7da38af41a2
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-09-20 18:01:10 +0530

    add eventlistner to button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,13 +6,16 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>
 {
-
+const clickHandler = () => {
+    console.log('clicked');
+};
 
    return (
          
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+        <button onClick={clickHandler}>start</button>
 
 
     </Card>
